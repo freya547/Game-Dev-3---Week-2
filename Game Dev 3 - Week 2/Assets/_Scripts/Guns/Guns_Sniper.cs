1d97e131ec4618b6569749d55e1dd84c81fb8927
freya547 <freyashilton@gmail.com>
2024-10-03 11:53:01 +0100

    Begin setup of Absolution gun

---
+++ Game Dev 3 - Week 2/Assets/_Scripts/Guns/Guns_Sniper.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameDevWithMarco.Guns
+{
+    public class Guns_Sniper : Guns_Parent
+    {
+        public override void GunSound()
+        {
+            gunAudioSource.pitch = Random.Range(0.6f, 1.4f);
+            gunAudioSource.PlayOneShot(gunSound);
+        }
+    }
+}
--- Game Dev 3 - Week 2/Assets/_Scripts/ScriptableObjects/GunsType_ScriptableObject.cs
+++ Game Dev 3 - Week 2/Assets/_Scripts/ScriptableObjects/GunsType_ScriptableObject.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO.Enumeration;
 using UnityEngine;
 using UnityEngine.UI;
 
